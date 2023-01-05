f3584a0ea9464b0b11490013daf5e238e7c5a3cd
Mateusz <mati123q@o2.pl>
2023-01-05 14:21:22 +0100

    Finished Nav

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,14 @@ const nav = document.querySelector('.nav')
 const burgerBtn = document.querySelector('.burger-btn')
 const main = document.querySelector('main')
 const mobileNav = document.querySelector('.nav-links-container-mobile')
+const navLinksMobile = document.querySelectorAll('.nav-link-mobile')
 
 const handleMobileNav = () => {
     mobileNav.classList.toggle('show-nav')
 }
+
+navLinksMobile.forEach(link => {
+    link.addEventListener('click',handleMobileNav)
+})
+
 burgerBtn.addEventListener('click', handleMobileNav)
