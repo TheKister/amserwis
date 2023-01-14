5246a7443b403d05449ddbbc5523cc2d2c7d3f56
Mateusz <mati123q@o2.pl>
2023-01-14 14:29:17 +0100

    finishing website

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,20 @@ const burgerBtn = document.querySelector('.burger-btn')
 const main = document.querySelector('main')
 const mobileNav = document.querySelector('.nav-links-container-mobile')
 const navLinksMobile = document.querySelectorAll('.nav-link-mobile')
+const footerYear= document.querySelector('.footer-year')
 
 const handleMobileNav = () => {
     mobileNav.classList.toggle('show-nav')
+}
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear()
+    footerYear.innerText=year
 }
 
 navLinksMobile.forEach(link => {
     link.addEventListener('click',handleMobileNav)
 })
 
+handleCurrentYear()
 burgerBtn.addEventListener('click', handleMobileNav)
